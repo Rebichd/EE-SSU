786d2004275d46300d0d7d8f5a2f93eaf0ca9e17
Rebichd <146671354+Rebichd@users.noreply.github.com>
2023-10-16 12:39:43 -0700

    Delete Networking/Intro_to_Linux_Shell_Scripting/outTest.sh

--- Networking/Intro_to_Linux_Shell_Scripting/outTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Author: Daniel Rebich
-#Sept. 14, 2023 - Version 1
-#----------------------------
-for((i=1; i<1000+1; i++));do
-	echo $i >> ourTest_1.txt;
-done
-
-tail -10 ourTest_1.txt > ourTest_2.txt;
