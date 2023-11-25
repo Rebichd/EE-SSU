5a242d3dd7fc4dbdc39e5b99c69297916fa8141d
Rebichd <146671354+Rebichd@users.noreply.github.com>
2023-11-25 10:32:41 -0800

    Create index2.php

---
+++ Networking/Creating_Servers_on_RPi/index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+        <head>
+                <title>Example</title>
+        </head>
+        <body>
+                <?php
+                        echo "Hi, I'm a PHP script!";
+                        echo date('Y-m-d H:i:s');
+                        phpinfo();
+                ?>
+        </body>
+</html>
