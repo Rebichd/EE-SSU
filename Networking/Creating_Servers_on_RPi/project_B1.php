22aa7b3f4a42427084a4f05778445566d3b4951d
Rebichd <146671354+Rebichd@users.noreply.github.com>
2023-11-25 10:33:21 -0800

    Create project_B1.php

---
+++ Networking/Creating_Servers_on_RPi/project_B1.php
@@ -0,0 +1,10 @@
+<?php
+        $txt ="Learn PHP";
+        $txt2 = "Engineering @ SSU";
+        $x = 5;
+        $y = 4;
+
+        echo $txt. "<br>";
+        echo "Study PHP at <br>" .$txt2. "<br>";
+        echo "the sum of X and Y is: <br>" .($x + $y);
+?>
