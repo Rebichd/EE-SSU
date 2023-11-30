3dba459615f203d5b5bf428803f563a15920452d
Rebichd <146671354+Rebichd@users.noreply.github.com>
2023-11-30 09:22:01 -0800

    Update controlRPI.py

--- Networking/Intro_to_ThingSpeak/controlRPI.py
+++ Networking/Intro_to_ThingSpeak/controlRPI.py
@@ -5,8 +5,8 @@ import string
 import json
 from time import sleep
 
-my_api_Key = "QDK4JCYV52H0129B"
-my_talkback_key = "38115193"
+my_api_Key = "****************"
+my_talkback_key = "********"
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -15,7 +15,7 @@ GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT)
 
 while True:
-        baseurl = ('https://api.thingspeak.com/talkbacks/50731/commands/execute.json?api_key=QDK4JCYV52H0129B')
+        baseurl = ('https://api.thingspeak.com/talkbacks/*****/commands/execute.json?api_key=****************')
         contents = json.loads(urllib.request.urlopen(baseurl).read())
 
         if len(contents) > 0:
